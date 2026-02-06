91f25cca119341fbbb1ee555fe853d37d39df5a7
Pedro Queiroga <pedroqueirogahub@gmail.com>
2026-02-06 17:31:18 -0300

    criando condição para dias de Segunda a Quinta

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ function VerificarHorarioFuncionamento(data = new Date()){
 
     const diaDaSemana = data.getDay(); 
 
-    
+    const hora = data.getHours()
+    const minutos = data.getMinutes();
+
+    const horarioAtual = hora*80 + minutos;
+
+        let funcionamento = false;
+            if(diaDaSemana>=1 && diaDaSemana<=4)
+                {
+                    const funcionamentoInicial = 8*60;
+                    const funcionamentoFinal = 20*60;
+                    funcionamento = horarioAtual >=funcionamentoInicial && horarioAtual <=funcionamentoFinal;
+                }
+            
+                
 }
 
 
